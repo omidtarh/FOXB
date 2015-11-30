89be578803b00eac3f8365b6346c11be0212c62e
mrmohammad <kashme.shab@gmail.com>
2015-11-30 18:07:53 +0330

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -217,7 +217,7 @@ function create_config( )
   "plugins",
   "stats",
   "version"},
-  sudo_users = {125489381},
+  sudo_users = {172767635},
   disabled_channels = {},
   moderation = {data = 'data/moderation.json'}
   }
